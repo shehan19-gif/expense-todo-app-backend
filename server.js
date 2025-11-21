3197e038eac2180d917f2370fedb935bfce436c3
shehan19-gif <shehanmaduwantha84@gmail.com>
2025-11-21 22:12:09 +0530

    cors updated

--- server.js
+++ server.js
@@ -27,7 +27,13 @@ app.use(express.urlencoded({extended: true}));
     origin: "http://localhost:5173",
     credentials: true,
 })); */
-app.use(cors());
+
+const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
+
+app.use(cors({
+    origin: allowedOrigin,
+    credentials: true,
+}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
